cd77c87454f121e80861c6413bfc00f00c4902b1
Stepan <stevedekart2020@gmail.com>
2024-11-21 00:18:47 +0100

    completed

--- config.php
+++ config.php
@@ -10,6 +10,8 @@ define('TELEGRAM_BOT_TOKEN', '7999759359:AAGbsMbXlPIndYCWAfcoFZn5BV-qwIGWX34');
 // Home url (without / on the end)
 define('HOME_URL', 'http://localhost:8000');
 
+// Home url (without / on the end)
+define('CV_URL', 'http://google.com');
 
 // Chat ID for Telegram Bot
 define('TELEGRAM_BOT_CHATID', '1121762916');
