1bc23b8ea05abaad94ba096422fb17864d4dc1db
stepan323446 <stevedekart2020@gmail.com>
2024-11-21 09:11:02 +0100

    readme

--- config-default.php
+++ config-default.php
@@ -1,8 +1,6 @@
 <?php
-
 // Server timezone for correct time
 define('SERVER_TIMEZONE', 'Europe/Belgrade');
-
 
 // Telegram bot token for sending messages
 define('TELEGRAM_BOT_TOKEN', '<BOT_TOKEN>');
@@ -10,10 +8,9 @@ define('TELEGRAM_BOT_TOKEN', '<BOT_TOKEN>');
 // Home url (without / on the end)
 define('HOME_URL', 'http://localhost:8000');
 
-// Home url (without / on the end)
+// Link on homepage to your CV
 define('CV_URL', 'http://google.com');
 
 // Chat ID for Telegram Bot
 define('TELEGRAM_BOT_CHATID', '<CHAT_ID_BOT>');
-
 ?>
